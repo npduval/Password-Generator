dafbd39a1dad013ca6a50c4139fd6d813158e85b
npduval <nickman92186@yahoo.com>
2021-09-29 20:01:59 -0400

    adding style.css to repository

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+const specialChar = ["!","\"","#","$","%","&","'","(",")","<","=",">","?","@","[","]"];
+const numericalChar = [1,2,3,4,5,6,7,8,9,0];
+const lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n"];
+const uppercaseChar = ["O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+
+// Prompts for perameters of new password
+function  prompts() {
+    var numberPrompt = prompt("How many characters would you like included in your password?")
+    const specialPrompt = confirm("Click OK to include special characters")
+    const numericalPrompt = confirm("Click OK to include numerical characters")
+    const lowercasePrompt= confirm("Click OK to include lowercase characters")
+    const uppercasePrompt = confirm("Click OK to include uppercase characters")
+    console.log(numberPrompt)
+    console.log(numericalPrompt)
+    console.log(lowercasePrompt)
+    console.log(uppercasePrompt)
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    const password = generatePassword();
+    const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
 
+
+//Add function for generatePassword
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-const numberChar = prompt("How many characters would you like included in your password?")
-const specialChar = confirm("Click OK to include special characters")
-const numericalChar = confirm("Click OK to include numerical characters")
-const lowercaseChar = confirm("Click OK to include lowercase characters")
-const uppercaseChar = copnfirm("Click OK to include uppercase characters")
+generateBtn.addEventListener("click", prompts);
+
